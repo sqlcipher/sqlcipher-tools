30102acd1529c702e23a5805ea2b3ee4bd9a451c
Billy Gray <wgray@zetetic.net>
2013-06-19 15:33:47 -0400

    use a stmt and bind parameters for attach so we're not repeating file path and key strings all over

--- attach.c
+++ attach.c
@@ -1,6 +1,10 @@
 /*
+ * rm *.db
  * cc attach.c -I../sqlcipher-static/sqlcipher -DSQLITE_HAS_CODEC -DSQLCIPHER_CRYPTO_CC -framework Security -DSQLITE_ENABLE_FTS3=1 -DSQLITE_TEMP_STORE=2 -o attach
- *
+ * OR
+ * cc attach.c -I../sqlcipher-static/sqlcipher -DSQLITE_HAS_CODEC -DSQLITE_ENABLE_FTS3=1 -DSQLITE_TEMP_STORE=2 -l crypto -o attach
+ * // -I../sqlcipher/src 
+ * ./attach
  */
 
 #include <sqlite3.c>
@@ -16,10 +20,12 @@ int main( int argc, const char* argv[])
   const char* localPath = "./attach-local.db";
   const char* remotePath = "./attach-remote.db";
   const char* rekeyPath = "./attach-rekey.db";
+  const char* attach_sql = "ATTACH DATABASE ? AS rekey KEY ?;";
   int rc;
-
   sqlite3 *local_db, *remote_db;  
   char *errorPointer;
+  sqlite3_stmt *stmt;
+  
   // create the remote db with legacy settings
   if (sqlite3_open(remotePath, &remote_db) != SQLITE_OK) {
     ERROR(("sqlite3_open failed for %s: %s\n", remotePath, sqlite3_errmsg(remote_db)))
@@ -52,20 +58,30 @@ int main( int argc, const char* argv[])
   sqlite3_key(remote_db, key, strlen(key));
   sqlite3_exec(remote_db, "PRAGMA kdf_iter = 4000;", NULL, 0, NULL);
   if (sqlite3_exec(remote_db, "SELECT * FROM sqlite_master;", NULL, 0, &errorPointer) != SQLITE_OK) {
-    ERROR(("select from sqlite_master failed: %s", errorPointer));
+    ERROR(("select from sqlite_master failed: %s\n", errorPointer));
     sqlite3_free(errorPointer);
     exit(1);
   }
-  // FIXME: use variables ...
-  if (sqlite3_exec(remote_db, "ATTACH DATABASE './attach-rekey.db' AS rekey KEY '123';", NULL, 0, &errorPointer) != SQLITE_OK) {
-    ERROR(("rekey attach failed: %s", errorPointer));
-    sqlite3_free(errorPointer);
+  // prepare stmt for attach
+  rc = sqlite3_prepare_v2(remote_db, attach_sql, -1, &stmt, NULL);
+  if (rc != SQLITE_OK) {
+    ERROR(("sqlite3_prepare_v2 failed for %s: %s\n", attach_sql, sqlite3_errmsg(remote_db)))
     exit(1);
   }
+  // "ATTACH DATABASE ? AS rekey KEY ?;";
+  sqlite3_bind_text(stmt, 1, rekeyPath, -1, SQLITE_TRANSIENT);
+  sqlite3_bind_text(stmt, 2, key, -1, SQLITE_TRANSIENT);
+  if (sqlite3_step(stmt) != SQLITE_DONE) {
+    ERROR(("rekey attach failed: %s\n", sqlite3_errmsg(remote_db)));
+    sqlite3_free(errorPointer);
+    sqlite3_finalize(stmt);
+    exit(1);
+  }
+  sqlite3_finalize(stmt);
   sqlite3_exec(remote_db, "PRAGMA rekey.kdf_iter = 64000;", NULL, 0, NULL);
   rc = sqlite3_exec(remote_db, "SELECT sqlcipher_export('rekey');", NULL, 0, NULL);
   if (rc != SQLITE_OK) {
-    ERROR(("sqlcipher_export failed: %s", sqlite3_errmsg(remote_db)))
+    ERROR(("sqlcipher_export failed: %s\n", sqlite3_errmsg(remote_db)))
     exit(1);
   }
   sqlite3_close(remote_db);
@@ -77,16 +93,27 @@ int main( int argc, const char* argv[])
   sqlite3_key(local_db, key, strlen(key));
   sqlite3_exec(local_db, "PRAGMA kdf_iter = 64000;", NULL, 0, NULL);
   if (sqlite3_exec(local_db, "SELECT * FROM sqlite_master;", NULL, 0, &errorPointer) != SQLITE_OK) {
-    ERROR(("select from sqlite_master failed: %s", errorPointer));
+    ERROR(("select from sqlite_master failed: %s\n", errorPointer));
     sqlite3_free(errorPointer);
     exit(1);
   }
+  // prepare stmt for attach on local_db
   // This is the heart of our test: does attaching this rekey'd db succeed or fail?
-  if (sqlite3_exec(local_db, "ATTACH DATABASE './attach-rekey.db' AS rekey KEY '123';", NULL, 0, &errorPointer) != SQLITE_OK) {
-    ERROR(("attach rekey to local failed: %s", errorPointer));
-    sqlite3_free(errorPointer);
+  rc = sqlite3_prepare_v2(local_db, attach_sql, -1, &stmt, NULL);
+  if (rc != SQLITE_OK) {
+    ERROR(("sqlite3_prepare_v2 failed for %s: %s\n", attach_sql, sqlite3_errmsg(remote_db)))
     exit(1);
   }
+  // "ATTACH DATABASE ? AS rekey KEY ?;";
+  sqlite3_bind_text(stmt, 1, rekeyPath, -1, SQLITE_TRANSIENT);
+  sqlite3_bind_text(stmt, 2, key, -1, SQLITE_TRANSIENT);
+  if (sqlite3_step(stmt) != SQLITE_DONE) {
+    ERROR(("rekey attach to local failed: %s\n", sqlite3_errmsg(local_db)));
+    sqlite3_free(errorPointer);
+    sqlite3_finalize(stmt);
+    exit(1);
+  }
+  sqlite3_finalize(stmt);
   sqlite3_close(local_db);
     
   return 0;
